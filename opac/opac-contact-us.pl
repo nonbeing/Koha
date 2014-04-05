7db5fa3191dfbc64469ca74ff2d777e0eb433a7d
Ambar Seksena <aseksena@qubole.com>
2014-04-05 17:43:00 +0530

    Added 'Contact us' to masthead.inc and corresponding pl and tt (template) files

---
+++ opac/opac-contact-us.pl
@@ -0,0 +1,49 @@
+#!/usr/bin/perl
+# WARNING: 4-character tab stops here
+
+# Copyright 2000-2002 Katipo Communications
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use strict;
+use warnings;
+
+use CGI;
+use C4::Auth;
+
+use C4::Context;
+use C4::Auth;
+use C4::Output;
+use C4::AuthoritiesMarc;
+use C4::Koha;    
+
+
+my $input = new CGI;
+my $query = $input->param('q');
+
+my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
+    {   
+        template_name   => "opac-contact-us.tmpl",
+        query           => $query,
+        type            => "opac",
+        authnotrequired => ( C4::Context->preference("OpacPublic") ? 1 : 0 ),
+        debug   => 1,
+    }   
+);
+
+$template->param( authtypesloop => \@authtypesloop );
+
+output_html_with_http_headers $query, $cookie, $template->output;
