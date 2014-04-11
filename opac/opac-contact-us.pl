664835d814a1c7be53911c059b7ada3913d3d50c
Ambar Seksena <aseksena@qubole.com>
2014-04-11 13:08:49 +0530

    ContactUs feature complete. Optional phone field added.

--- opac/opac-contact-us.pl
+++ opac/opac-contact-us.pl
@@ -28,29 +28,85 @@ use C4::Context;
 use C4::Auth;
 use C4::Output;
 use C4::AuthoritiesMarc;
-use C4::Koha;    # XXX subfield_is_koha_internal_p
+use C4::Koha;
+
+use Email::Send;
+use Email::Send::Gmail;
+use Email::Simple::Creator;
+
+
+sub sendGmail {
+    my ($name, $email_address, $phone, $subject, $message) = @_;
+
+    my $body = "Dear Librarian,\nThis Message was received from the OPAC Contact Us page:\n\n\n" .
+      "ENQUIRER'S NAME: \n$name\n\n\nENQUIRER'S EMAIL: \n$email_address\n\n\nENQUIRER'S PHONE: \n$phone" .
+      "\n\n\nENQUIRER'S MESSAGE: \n$message\n\n\n";
+
+    my $email = Email::Simple->create(
+      header => [
+          From    => 'LibraryContactUs@gmail.com',
+          #To      => 'ambarseksena@gmail.com',
+          To      => 'library@vvmvp.org',
+          Subject => $subject,
+      ],
+      body => $body,
+    );
+
+    my $sender = Email::Send->new(
+      {   mailer      => 'Gmail',
+          mailer_args => [
+              username => 'LibraryContactUs@gmail.com',
+              password => 'JGDkohaAOL13may',
+          ]
+      }
+    );
+
+    eval { $sender->send($email) };
+    die "Error sending email: $@" if $@;
+}
 
 my $query        = new CGI;
 my $op           = $query->param('op') || '';
-my $authtypecode = $query->param('authtypecode') || '';
-my $dbh          = C4::Context->dbh;
 
 my ( $template, $loggedinuser, $cookie );
+my ( $name, $email_address, $phone, $subject, $message);
 
-( $template, $loggedinuser, $cookie ) = get_template_and_user(
+
+if ( $op eq "do_submit" ) {
+    ( $template, $loggedinuser, $cookie ) = get_template_and_user(
         {
-            #template_name   => "opac-authorities-home.tmpl",
-            template_name   => "opac-contact-us.tmpl",
+            template_name   => "opac-contact-us-submitted.tmpl",
             query           => $query,
             type            => 'opac',
-            authnotrequired => ( C4::Context->preference("OpacPublic") ? 1 : 0 ),
+            authnotrequired => 1,
             debug           => 1,
         }
     );
 
+    $name = $query->param('Name');
+    $email_address = $query->param('Email');
+    $phone = $query->param('Phone');
+    $subject = $query->param('Subject');
+    $message = $query->param('Message');
 
-
+    if ( defined($email_address)) {
+        sendGmail($name, $email_address, $phone, $subject, $message);
+    }
+}
+else {
+    ( $template, $loggedinuser, $cookie ) = get_template_and_user(
+            {
+                template_name   => "opac-contact-us.tmpl",
+                query           => $query,
+                type            => 'opac',
+                authnotrequired => ( C4::Context->preference("OpacPublic") ? 1 : 0 ),
+                debug           => 1,
+            }
+        );
+}
 # Print the page
 output_html_with_http_headers $query, $cookie, $template->output;
+
+
 
 
